9e2844e91086e5b1200c6cb8c52220fb9019c834
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-07-02 16:25:12 +0800

    home sheet structure

--- QuickRoom/UI/Home/HomeView.swift
+++ QuickRoom/UI/Home/HomeView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct HomeView: View {
-	// TODO: Home view UI
+	@State private var isSheetShown: Bool = true
+	@State private var currentSheetDetent: PresentationDetent = .medium // TODO: Decide default
+	
     var body: some View {
-		Text("This is home")
+		VStack {
+			Text("TODO: MAP")
+		}
+		.frame(maxWidth: .infinity, maxHeight: .infinity) // TODO: Remove
+		.background(.red) // TODO: Remove
+		.sheet(isPresented: $isSheetShown) {
+			Text("TODO: Sheet")
+				.presentationDetents(
+					[.height(80), .medium, .large], // TODO: Define the lowest height
+					selection: $currentSheetDetent
+				)
+				.presentationBackgroundInteraction(.enabled)
+				.interactiveDismissDisabled(true)
+				.presentationDragIndicator(.visible)		}
     }
 }
 
