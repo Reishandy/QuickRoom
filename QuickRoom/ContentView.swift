ceb4d9b051bef86b6aeb8f576c28c78059bdcb66
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-07-02 09:19:18 +0800

    Initial Commit

---
+++ QuickRoom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuickRoom
+//
+//  Created by Muhammad Akbar Reishandy on 02/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuickRoom/QuickRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickRoomApp.swift
+//  QuickRoom
+//
+//  Created by Muhammad Akbar Reishandy on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct QuickRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
